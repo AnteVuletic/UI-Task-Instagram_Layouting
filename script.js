9ecaca1c6bbcb8884407726fcfa824fc8cefad57
AnteVuletic <antevuletic1995@gmail.com>
2019-03-06 10:32:23 +0100

    Flex done except iphone breakpoint.

---
+++ script.js
@@ -0,0 +1,19 @@
+let item = document.querySelectorAll(".content__item");
+let modal = document.querySelector(".modal__wrapper");
+let modalImage = document.querySelector(".modal__image")
+
+document.addEventListener("click",(event)=>{
+    if(modal.classList.contains('modal__wrapper-visible')){
+        if(event.target === modal){
+            modal.classList.remove('modal__wrapper-visible');
+        }
+    }
+    for(let element of item){
+        if(event.target === element.childNodes[3]){
+            const imageTargeted = element.childNodes[1].childNodes[1].cloneNode(true);
+            modal.classList.add('modal__wrapper-visible');
+            modalImage.appendChild(imageTargeted);
+        }
+    }
+    return event;
+});
